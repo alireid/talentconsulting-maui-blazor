d86f28a174019d3608867a83f63a0289ecd31d8c
Alasdair Reid <ali.reid@live.co.uk>
2022-10-20 08:29:43 +0100

    Initial commit

---
+++ TalentConsulting.MauiBlazor/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TalentConsulting.MauiBlazor;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => TalentConsultingProgram.CreateMauiApp();
+}
